13efe0f3dbb9e9d614e877913a04556ed483a388
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-08 17:08:24 -0500

    Build initial elements and styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,26 @@
+import styles from "./Home.module.css";
+
+
 export default function Home() {
     return (
-        <div>Home Test</div>
+        <section>
+            <article className={styles.heroContainer}>
+                <p className={styles.heroText}>Attention-Grabbing Text</p>
+                <div className={styles.imageContainer}>
+                    <div className={styles.vignette}></div>
+                    <picture>
+                        <source media="(width < 600px)" srcSet="/src/assets/images/hero-image-mobile.jpg"/>
+                        <source media="(width > 600px)" srcSet="/src/assets/images/hero-image.jpg" />
+                        <img src="/src/assets/images/hero-image.jpg" alt="hero image" />
+                    </picture>
+                </div>
+            </article>
+            <article>
+                <p>More text about our mission</p>
+                <div>
+                    <button>Shop Now</button>
+                </div>
+            </article>
+        </section>
     )
 }
