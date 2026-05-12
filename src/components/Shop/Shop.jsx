c56ac9fe3656100cd4d3bde8a8fe5c91f26d158b
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-12 16:03:52 -0500

    feat: Build static Shop component

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,5 +1,30 @@
+import Card from "../Card/Card.jsx";
+
+import styles from "./Shop.module.css";
+
 export default function Shop() {
     return (
-        <div>Shop Test</div>
+        <section className={styles.content}>
+            <h3 className={styles.title}>Women's</h3>
+            <div className={styles.container}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            <h3 className={styles.title}>Men's</h3>
+            <div className={styles.container}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+            <h3 className={styles.title}>Jewelry</h3>
+            <div className={styles.container}>
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </section>
     )
 }
