379e794886505df8a1f4df6a0a257c00e130f36d
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-13 12:19:18 -0500

    feat: Pass api data to card components

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,29 +1,30 @@
 import Card from "../Card/Card.jsx";
+import { useOutletContext } from "react-router";
 
 import styles from "./Shop.module.css";
 
 export default function Shop() {
+    const { womensData, mensData, jewelryData } = useOutletContext();
+
     return (
         <section className={styles.content}>
             <h3 className={styles.title}>Women's</h3>
             <div className={styles.container}>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {womensData.map((data) => (
+                    <Card key={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} />
+                ))}
             </div>
             <h3 className={styles.title}>Men's</h3>
             <div className={styles.container}>
-                <Card />
-                <Card />
-                <Card />
+                {mensData.map((data) => (
+                    <Card key={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} />
+                ))}
             </div>
             <h3 className={styles.title}>Jewelry</h3>
             <div className={styles.container}>
-                <Card />
-                <Card />
-                <Card />
+            {jewelryData.map((data) => (
+                    <Card key={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} />
+                ))}
             </div>
         </section>
     )
