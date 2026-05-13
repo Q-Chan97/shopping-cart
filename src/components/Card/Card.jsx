c074e8197d3ff2425d89d827169b68f66e870082
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-13 12:19:51 -0500

    feat: Pass props to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,19 +1,19 @@
 import styles from "./Card.module.css";
 
-export default function Card() {
+export default function Card({ image, title, price, rating, count }) {
     return (
         <article className={styles.cardContainer}>
-            <img className={styles.image} src="./src/assets/images/dummy-image.png" alt="product image" />
+            <div className={styles.imgContainer}><img className={styles.image} src={image} alt="product image" /></div>
             <div>
-                <h4>Product Name</h4>
+                <h4 className={styles.title}>{title}</h4>
                 <div className={styles.rating}>
                     <div>
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m233-120 65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Z"/></svg>
-                        <p>4.3</p>
+                        <p>{rating}</p>
                     </div>
-                    <p>(5k)</p>
+                    <p>({count})</p>
                 </div>
-                <p className={styles.price}>$9.99</p>
+                <p className={styles.price}>${price}</p>
             </div>
             <div className={styles.quantityContainer}>
                 <button type="button" className={styles.button}>
