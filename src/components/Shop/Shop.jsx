acfb1b7317bdbec6075470e87a625e1ceff82ea9
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-13 16:10:34 -0500

    feat: Pass addToCart function

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -4,26 +4,26 @@ import { useOutletContext } from "react-router";
 import styles from "./Shop.module.css";
 
 export default function Shop() {
-    const { womensData, mensData, jewelryData } = useOutletContext();
+    const { womensData, mensData, jewelryData, addToCart } = useOutletContext();
 
     return (
         <section className={styles.content}>
             <h3 className={styles.title}>Women's</h3>
             <div className={styles.container}>
                 {womensData.map((data) => (
-                    <Card key={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} />
+                    <Card key={data.id} id={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} addToCart={addToCart} />
                 ))}
             </div>
             <h3 className={styles.title}>Men's</h3>
             <div className={styles.container}>
                 {mensData.map((data) => (
-                    <Card key={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} />
+                    <Card key={data.id} id={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} addToCart={addToCart} />
                 ))}
             </div>
             <h3 className={styles.title}>Jewelry</h3>
             <div className={styles.container}>
             {jewelryData.map((data) => (
-                    <Card key={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} />
+                    <Card key={data.id} id={data.id} image={data.image} title={data.title} price={data.price} rating={data.rating.rate} count={data.rating.count} addToCart={addToCart} />
                 ))}
             </div>
         </section>
