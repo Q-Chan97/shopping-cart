3fce6b371926067a5a8b3f27ed82ea1b5eb842a9
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-14 12:17:15 -0500

    feat: Create empty version of component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,7 +4,7 @@ import styles from "./Cart.module.css";
 import CartCard from "../CartCard/CartCard.jsx";
 
 export default function Cart() {
-    const { cart, setCart } = useOutletContext();
+    const { cart, setCart, } = useOutletContext();
 
     function calculateSubtotal(item) {
         return (item.price * item.quantity).toFixed(2);
@@ -16,21 +16,29 @@ export default function Cart() {
         }, 0).toFixed(2);
     }
 
+    const isPopulated = cart.length > 0;
+
     return (
-        <section className={styles.pageContainer}>
-            <h3>Your cart</h3>
-            <div className={styles.cartContainer}>
-                {cart.map((product) => (
-                    <CartCard key={product.id} id={product.id} image={product.image} title={product.title} price={product.price} quantity={product.quantity} subtotal={calculateSubtotal(product)} />
-                ))}
-            </div>
-            <div className={styles.totalContainer}>
-                <p>Total:</p>
-                <p>${calculateTotal(cart)}</p>
-            </div>
-            <div className={styles.checkoutDiv}>
-                <p>Checkout (unavailable)</p>
-            </div>
-        </section>
+        isPopulated ? (
+            <section className={styles.pageContainer}>
+                <h3>Your cart</h3>
+                <div className={styles.cartContainer}>
+                    {cart.map((product) => (
+                        <CartCard key={product.id} id={product.id} image={product.image} title={product.title} price={product.price} quantity={product.quantity} subtotal={calculateSubtotal(product)} />
+                    ))}
+                </div>
+                <div className={styles.totalContainer}>
+                    <p>Total:</p>
+                    <p>${calculateTotal(cart)}</p>
+                </div>
+                <div className={styles.checkoutDiv}>
+                    <p>Checkout (unavailable)</p>
+                </div>
+            </section>
+        ) : (
+            <section className={styles.pageContainer}>
+                <h3>Your cart appears to be empty.</h3>
+            </section>
+        )
     )
 }
