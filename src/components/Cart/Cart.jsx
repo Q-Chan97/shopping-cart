28002aab75ea2103d5f9403f8fe48519324ba3d9
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-14 10:46:24 -0500

    feat: Build static Cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,5 +1,28 @@
+import { useOutletContext } from "react-router";
+
+import styles from "./Cart.module.css";
+import CartCard from "../CartCard/CartCard.jsx";
+
 export default function Cart() {
+    const { cart, setCart } = useOutletContext();
+
+    console.log(cart);
+
     return (
-        <div>Cart Test</div>
+        <section className={styles.pageContainer}>
+            <h3>Your cart</h3>
+            <div className={styles.cartContainer}>
+                {cart.map((product) => (
+                    <CartCard key={product.id} id={product.id} image={product.image} title={product.title} price={product.price} quantity={product.quantity} />
+                ))}
+            </div>
+            <div className={styles.totalContainer}>
+                <p>Total:</p>
+                <p>$129.54</p>
+            </div>
+            <div className={styles.checkoutDiv}>
+                <p>Checkout (unavailable)</p>
+            </div>
+        </section>
     )
 }
