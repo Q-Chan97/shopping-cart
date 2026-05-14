2581948107117180816de7175e549a4708439746
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-14 12:04:27 -0500

    feat: Create total and item subtotal functions

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,19 +6,27 @@ import CartCard from "../CartCard/CartCard.jsx";
 export default function Cart() {
     const { cart, setCart } = useOutletContext();
 
-    console.log(cart);
+    function calculateSubtotal(item) {
+        return (item.price * item.quantity).toFixed(2);
+    }
+
+    function calculateTotal(cart) {
+        return cart.reduce((total, item) => {
+            return total + (item.price * item.quantity);
+        }, 0).toFixed(2);
+    }
 
     return (
         <section className={styles.pageContainer}>
             <h3>Your cart</h3>
             <div className={styles.cartContainer}>
                 {cart.map((product) => (
-                    <CartCard key={product.id} id={product.id} image={product.image} title={product.title} price={product.price} quantity={product.quantity} />
+                    <CartCard key={product.id} id={product.id} image={product.image} title={product.title} price={product.price} quantity={product.quantity} subtotal={calculateSubtotal(product)} />
                 ))}
             </div>
             <div className={styles.totalContainer}>
                 <p>Total:</p>
-                <p>$129.54</p>
+                <p>${calculateTotal(cart)}</p>
             </div>
             <div className={styles.checkoutDiv}>
                 <p>Checkout (unavailable)</p>
