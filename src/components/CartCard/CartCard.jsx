32dbc3c52807853f715a144a1255d8aa3083df34
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2026-05-14 12:29:31 -0500

    feat: Wire listener for removeFromCart

--- src/components/CartCard/CartCard.jsx
+++ src/components/CartCard/CartCard.jsx
@@ -1,6 +1,6 @@
 import styles from "./CartCard.module.css";
 
-export default function CartCard({ id, image, title, quantity, subtotal }) {
+export default function CartCard({ image, title, quantity, subtotal, remove }) {
     return (
         <div className={styles.container}>
             <div className={styles.imgContainer}>
@@ -12,7 +12,7 @@ export default function CartCard({ id, image, title, quantity, subtotal }) {
             </div>
             <div className={styles.infoContainer}>
                 <p className={styles.total}>Item total: ${subtotal}</p>
-                <button aria-label="remove item" type="button" className={styles.remove}>
+                <button aria-label="remove item" type="button" className={styles.remove} onClick={remove}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#cd3d3d"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/></svg>
                 </button>
             </div>
